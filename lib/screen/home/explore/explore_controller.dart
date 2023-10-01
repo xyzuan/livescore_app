a7c5cb6311daca8804db9cc7193c009077200922
xyzuan <xyzuannihboss@gmail.com>
2023-10-02 02:07:49 +0700

    main: Add Search in Explore screen

--- lib/screen/home/explore/explore_controller.dart
+++ lib/screen/home/explore/explore_controller.dart
@@ -1,3 +1,9 @@
 import 'package:get/get.dart';
 
-class ExploreController extends GetxController {}
+class ExploreController extends GetxController {
+  RxInt selectedIndex = 0.obs;
+
+  void handleCategorySelection(int index) {
+    selectedIndex.value = index;
+  }
+}
